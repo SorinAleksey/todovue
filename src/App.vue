3cf164e1d2483e7c44651cea30bdb3b3f9cc9dc0
SorinAleksey <aleksey.v.sorin@gmail.com>
2024-02-05 07:56:59 +0300

    First changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <h4>To do</h4>
+  <div class="todos">
+    <todo-item 
+      v-for="todo in todos" 
+      :key="todo._id"
+      :_id="todo._id" 
+      :name="todo.name" 
+      :isCompleted="todo.isCompleted" 
+      :type="todo.type" 
+      @complSwap="completedHandler(todo._id)" 
+      @remove="removetodo(todo._id)"
+    >
+    </todo-item>
+  </div>
+
+  <div class="add-todo">
+    <input type="text" placeholder="todo name" v-model="todoName"/>
+
+    <select v-model="todoType" class="drop">
+      <option>type1</option>
+      <option>type2</option>
+    </select>
+    
+    <button @click="addtodo()">+</button>
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todoItem from './components/todoItem.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      todoName: "",
+      todoType: "type1",
+      todos: []
+    };
+  },
+  methods: {
+    completedHandler: function (todoId) {
+      const  currenttodo = this.todos.find((t) => t._id === todoId);
+      currenttodo.isCompleted = !currenttodo.isCompleted;
+    },
+    addtodo: function () {
+      this.todos.push(
+        {
+          _id: Math.random().toString(36).substring(4, 9),
+          name: this.todoName,
+          isCompleted: false,
+          type: this.todoType
+        }
+      );
+    },
+    removetodo: function (todoId) {
+      this.todos = this.todos.filter((t) => t._id !== todoId);
+    }
+  },
   components: {
-    HelloWorld
+    todoItem
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/todoItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div 
+      class="todo" 
+      :class="{completed: isCompleted}"
+      @click="$emit('complSwap')"
+    >
+      <div class="round" :class="classtodoType()"></div>
+      
+      <span>{{name}}</span>
+
+      <div class="remove" @click="$emit('remove')">
+        <div class="round2">
+          x
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-item',
+  props: ['_id', 'name', 'isCompleted', 'type'],
+  emits: ['remove', 'complSwap'],
+  methods: {
+    classtodoType: function () {
+      return {
+        type1: this.type === 'type1', 
+        type2: this.type === 'type2'
+      }
+    }
+  }
+}
+</script>
+
